288d938e5eec0c8704990ff7f6b74d75b6da176a
Darkgoatie <haliterenozkir@gmail.com>
2023-12-21 14:42:27 +0000

    Upd

--- src/index.js
+++ src/index.js
@@ -6,49 +6,114 @@ const axios = require("axios");
  * */
 class Mangadex {
   /**
-   * @param {Object} options
-   * @param {Boolean} [options.fetchOne=false] Fetches only one manga.  Function returns a single Manga objectt if true.
-   * @param {String} options.title Manga title to search for.
-   * @returns {Manga[]|Manga} Returns a single manga/manga array depending on fetchOne option.
+   * @param {String} authorId 
+   * @returns {Author}
    */
-  async fetchManga(options) {
-    if (options === undefined) {
-      throw new TypeError("Options undefined!");
-    }
+  async fetchAuthorByID(authorId) {
 
-    let fetchOne;
-    if (options.fetchOne === true) {
-      fetchOne = true;
-    } else {
-      fetchOne = false;
-    }
+  }
 
-    if (typeof options.title !== "string") {
-      throw new TypeError("Title should be string");
-    }
+  /**
+   * @typedef {Object} AuthorQuery 
+   * @property {String} name Author's name
+   * @property {Object} order Order parameters
+   * @property {String[]} includes
+   * @property {String[]} ids Author ids (Limited to 100 per request)
+   */
 
+  /**
+   * @param {AuthorQuery} query 
+   * @returns {Author[]}
+   */
+  async fetchAuthors(query) {
+    const resp = await axios({
+      method: "GET",
+      url: this.baseURL + "/author",
+      params: {
+        ...query
+      }
+    });
+
+
+
+
+  }
+  /**
+   * @typedef {("shounen"|"shoujo"|"josei"|"seinen"|"none")} publicationDemographic
+   */
+
+  /**
+   * @typedef {("ongoing"|"hiatus"|"completed"|"cancelled")} mangaStatus
+   */
+
+  /**
+   * @typedef {("safe"|"suggestive"|"erotica"|"pornographic")} contentRating
+   */
+
+  /**
+   * @typedef {Object} MangaQuery
+   * @property {String} title
+   * @property {String} authorOrArtist Author/Artist ID
+   * @property {String[]} authors
+   * @property {String[]} artists
+   * @property {Number} year
+   * @property {String[]} includedTags
+   * @property {String} [includedTagsMode="AND"] Avaliale values: AND, OR
+   * @property {String[]} excludedTags
+   * @property {String} [excludedTagsMode="OR"] Avaliable values: AND, OR
+   * @property {mangaStatus[]} status Avaliable values: "ongoing", "completed", "hiatus", "cancelled"
+   * @property {String[]} originalLanguage 
+   * @property {String[]} excludedOriginalLanguage
+   * @property {String[]} availableTranslatedLanguage
+   * @property {publicationDemographic[]} publicationDemographic
+   * @property {String[]} ids Manga ids (limited to 100 per request)
+   * @property {contentRating[]} [contentRating=["safe", "suggestive", "erotica"]]
+   * @property {String} createdAtSince
+   * @property {String} updatedAtSince
+   * @property {Object} order
+   * @property {String} [order.latestUploadedChapter="desc"]
+   * @property {String[]} includes Available values: "manga", "artist", "tag", "author", "creator", "cover_art"
+   * @property {String} hasAvaliableChapters Available values: "0", "1", "true", "false"
+   * @property {String} group Group ID
+   */
+
+  /**
+   * @param {MangaQuery} query
+   * @returns {Manga[]} Returns manga array
+   */
+  async fetchManga(query) {
     const resp = await axios({
       method: "GET",
       url: `${this.baseURL}/manga`,
       params: {
-        title: options.title,
+        ...query
       },
     });
 
     let results = resp.data.data;
 
-    if (fetchOne !== true) {
-      return results.map(m => new Manga(m));
-    } else {
-      return new Manga(results[0]);
-    }
+    return results.map(m => new Manga(m));
   }
 
-
   /**
-   * @param {String} baseURL Mangadex base API url. ("https://api.mangadex.org")
+   * @param {String} mangaId
+   * @returns {Manga}
    */
+  async fetchMangaByID(mangaId) {
+    const resp = await axios({
+      method: "GET",
+      url: this.baseURL + "/manga/" + mangaId,
+      params: {id: mangaId}
+    });
+
+    return new Manga(resp.data.data);
+  }
+
   constructor() {
+    /**
+     * base api url
+     * @type {"https://api.mangadex.org"}
+     */
     this.baseURL = "https://api.mangadex.org";
   }
 }
@@ -61,8 +126,7 @@ class Manga {
   /**
    * @param {Object} order Sorting/Filtering parameters
    * @param {String} [order.translatedLanguage="en"] The language of the chapters to be imported. See example link for codes.
-   * @example
-   * Check https://api.mangadex.org/docs/3-enumerations/ for allowed order params
+   * @example Check https://api.mangadex.org/docs/3-enumerations/ for allowed order params
    * @returns {Chapter[]} Fetched chapters
    */
   async getChapters(order) {
@@ -120,13 +184,17 @@ class Manga {
 
     /**
      * Base api URL ("https://api.mangadex.org")
-     * @type {String} 
+     * @type {"https://api.mangadex.org"} 
      */
     this.baseURL = "https://api.mangadex.org";
     /**
      * Manga title for languages
      * @type {LocalizedString}
      */
+    if(!mangadata.attributes) {
+      mangadata.attributes = mangadata
+    };
+
     this.title = mangadata.attributes.title;
     /**
      * Manga api ID
@@ -154,8 +222,8 @@ class Manga {
      */
     this.description = mangadata.attributes.description;
     /**
-     * Current manga status: "ongoing", "hiatus", "completed" or "canceled"
-     * @type {String} 
+     * Current manga status
+     * @type {mangaStatus} 
      */
     this.status = mangadata.attributes.status;
     /**
@@ -237,24 +305,73 @@ class Chapter {
  * @param {Object} chapterdata Raw API chapter data
  */
   constructor(chapterdata) {
+    if(!chapterdata.attributes) chapterdata.attributes = chapterdata;
+    /**
+     * Volume that the chapter belongs to
+     * @type {String} 
+     */
+    this.volume = chapterdata.attributes.volume
     /**
      * Chapter api ID
      * @type {String}
      */
     this.id = chapterdata.id;
-    
+    /**
+     * Title
+     * @type {String}
+     */
     this.title = chapterdata.attributes.title;
-    this.no = chapterdata.attributes.chapter;
+    /**
+     * @type {Number}
+     */
+    this.chapter = chapterdata.attributes.chapter;
+    /**
+     * @type {String} 
+     */
     this.translatedLanguage = chapterdata.attributes.translatedLanguage;
+    /**
+     * @type {String}
+     */
     this.publishAt = chapterdata.attributes.publishAt;
+    /**
+     * Number of pages
+     * @type {Number}
+     */
     this.pages = chapterdata.attributes.pages;
+    /**
+     * @type {"chapter"}
+     */
+    this.type = chapterdata.type
+    /**
+     * @type {String}
+     */
+    this.uploader = chapterdata.attributes.uploader;
+    /**
+     * @type {Number}
+     */
+    this.version = chapterdata.attributes.version
+    /**
+     * @type {String} 
+     */
+    this.readableAt = chapterdata.attributes.readableAt
+    /**
+     * @type {String}
+     */
+    this.updatedAt = chapterdata.attributes.updatedAt
+    /**
+     * @type {String}
+     */
+    this.createdAt = chapterdata.attributes.createdAt
+    /**
+     * @type {Relationship[]}
+     */
+    this.relationships = chapterdata.relationships
   }
 }
 
 const run = async () => {
   const md = new Mangadex();
-  const manga = (await md.fetchManga({ title: "Jujutsu Kaisen" }))[0];
-  console.log(manga);
+  const manga = new Manga((await md.fetchMangaByID()))
 };
 
 run();
